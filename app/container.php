28a0a520d123050ea4394398d40e768c63162e3a
SNebozuk <141873061+SNebozuk@users.noreply.github.com>
2026-07-30 20:21:51 -0300

    Simplificar servicios y registrar fotos de productos

--- app/container.php
+++ app/container.php
@@ -3,12 +3,11 @@ declare(strict_types=1);
 
 use LaboratorioDigital\Auth;
 use LaboratorioDigital\BackupService;
-use LaboratorioDigital\CashService;
 use LaboratorioDigital\MailService;
 use LaboratorioDigital\OrderService;
 use LaboratorioDigital\PaymentProofService;
 use LaboratorioDigital\ProductService;
-use LaboratorioDigital\ReportService;
+use LaboratorioDigital\ProductImageService;
 use LaboratorioDigital\SettingsService;
 use LaboratorioDigital\StockService;
 
@@ -18,16 +17,19 @@ require_once __DIR__ . '/Http.php';
 require_once __DIR__ . '/Auth.php';
 require_once __DIR__ . '/BackupService.php';
 require_once __DIR__ . '/ProductService.php';
+require_once __DIR__ . '/ProductImageService.php';
 require_once __DIR__ . '/StockService.php';
 require_once __DIR__ . '/OrderService.php';
 require_once __DIR__ . '/PaymentProofService.php';
-require_once __DIR__ . '/CashService.php';
 require_once __DIR__ . '/MailService.php';
-require_once __DIR__ . '/ReportService.php';
 require_once __DIR__ . '/SettingsService.php';
 
 $app['auth'] = new Auth($app['pdo']);
 $app['products'] = new ProductService($app['pdo']);
+$app['product_images'] = new ProductImageService(
+    $app['root'],
+    $app['config']
+);
 $app['stock'] = new StockService($app['pdo']);
 $app['orders'] = new OrderService(
     $app['pdo'],
@@ -39,8 +41,6 @@ $app['proofs'] = new PaymentProofService(
     $app['stock'],
     $app['config']
 );
-$app['cash'] = new CashService($app['pdo']);
-$app['reports'] = new ReportService($app['pdo']);
 $app['backups'] = new BackupService($app['pdo'], $app['config']);
 $app['mail'] = new MailService($app['pdo'], $app['config']);
 $app['settings'] = new SettingsService($app['pdo']);
