abc92d3f15ccd1ca3f9f280b9210b76cf422b9a1
Sergio Nebozuk <sergionebozuk@gmail.com>
2026-08-23 00:52:04 -0300

    Actualizar texto de portada

--- v1/index.php
+++ v1/index.php
@@ -126,7 +126,7 @@ header('Referrer-Policy: same-origin');
             </div>
             <div class="catalog-intro">
                 <h1 id="catalog-title">Insumos para sublimación <em>y DTF en Rosario.</em></h1>
-                <p>Papeles, indumentaria, sublimables y todo para personalizar. Precios mayoristas y retiro en el local.</p>
+                <p>Precios mayoristas sin límite de compra.</p>
                 <p class="catalog-slogan">Tus ideas merecen hacerse realidad.</p>
             </div>
 
